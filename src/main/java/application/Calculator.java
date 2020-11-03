60c450e57015f86e66d13f2b0c9218750a1761a7
Oleksandr Dziubaniuk <sashagood12121986@gmail.com>
2020-11-03 20:42:05 +0200

    initial pull

--- src/main/java/application/Calculator.java
+++ src/main/java/application/Calculator.java
@@ -34,24 +34,20 @@ public class Calculator {
         return Math.sqrt(a);
     }
 
-    //TODO add method for x^2
     public double power(double a, double b) {
         return Math.pow(a, b);
     }
 
     //***************Medium methods************************
 
-    //TODO (*) |average value| add method which calculate average value of 3 numbers
     public int averageValue(int a, int b, int c) {
         return (a + b + c) / 3;
     }
 
-    //TODO (*) |even or odd| add method which get 1 number as parameter and return true if it even, and false - if odd
     public boolean evenOrOddNumbers(int a) {
         return a % 2 == 0;
     }
 
-    //TODO (*) |biggest value| add method which get 2 number as parameters and return bigger of them, or 0 if they equals
     public int biggestValue(int a, int b) {
         if (a > b) {
             return a;
@@ -64,7 +60,6 @@ public class Calculator {
 
     //***************Hard methods************************
 
-    //TODO (**) |addition with multiple parameters| add method which return sum of any quantity of parameter
     public int sumOfParameters(int... num) {
         int sum = 0;
         for (double i : num)
@@ -72,16 +67,12 @@ public class Calculator {
         return sum;
     }
 
-    //TODO (*) |divide by zero case| add method for division (check division by zero case
-    // - if we try to divide by zero - return -100;
     public int divideByZeroCase(int a, int b) {
         if (b == 0) {
             return -100;
         }
         return a / b;
     }
-
-
 }
 
 
